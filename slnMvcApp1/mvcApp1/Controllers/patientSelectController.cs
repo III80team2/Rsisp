1e1a19da4d9cd6adab8855efc76a1f5925c6fa5e
HAHAHIGH <dkhigh0523@gmail.com>
2015-05-06 13:33:37 +0800

    UI01
    Test

---
+++ slnMvcApp1/mvcApp1/Controllers/patientSelectController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcApp1.Controllers
+{
+    public class patientSelectController : Controller
+    {
+        //
+        // GET: /patientSelect/
+        public ActionResult patientSelect()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
