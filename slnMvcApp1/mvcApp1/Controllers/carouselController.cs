2424d648cfbbd3e6de6da01528ec8d3a6403fe2e
HAHAHIGH <dkhigh0523@gmail.com>
2015-05-19 11:24:35 +0800

    重建UI01_2015/05/08 carousel

---
+++ slnMvcApp1/mvcApp1/Controllers/carouselController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcApp1.Controllers
+{
+    public class carouselController : Controller
+    {
+        //
+        // GET: /carousel/
+
+        public ActionResult carouselTest() 
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
