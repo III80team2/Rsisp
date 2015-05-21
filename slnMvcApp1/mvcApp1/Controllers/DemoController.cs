856205fe89ebc666656172edd3dde93f77dcdb38
HAHAHIGH <dkhigh0523@gmail.com>
2015-05-21 11:21:47 +0800

    UI01_2015/05/10 layout
    使用layout排版

---
+++ slnMvcApp1/mvcApp1/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcApp1.Controllers
+{
+    public class DemoController : Controller
+    {
+        //
+        // GET: /Demo/
+
+        public ActionResult DemoTest() 
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
