f47be6242a3a0e7b3ffa40cd43e65dd59fa875ef
easonsu80 <easonsu80@gmail.com>
2015-06-05 16:18:51 +0800

    MyAppTest2 地圖完成  送!!!!!!!!!2015/06/05

--- MyappTest2/app/src/main/java/com/example/danny/myapptest/ActMain.java
+++ MyappTest2/app/src/main/java/com/example/danny/myapptest/ActMain.java
@@ -107,6 +107,7 @@ public class ActMain extends Activity {
                 manager.notify(0, message);
 
                 Toast.makeText(ActMain.this,"歡迎使用威爵工作室系統",Toast.LENGTH_SHORT).show();
+                Toast.makeText(ActMain.this,"請注意通知欄",Toast.LENGTH_LONG).show();
 
 
 
@@ -160,9 +161,12 @@ public class ActMain extends Activity {
     View.OnClickListener btnMap_click=new View.OnClickListener(){
         public void onClick(View arg0) {
 
-            Uri uri = Uri.parse("geo:22.638692,120.397787,3a,75y,343.8h,71.11t");	//經緯度
-            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//            Uri uri = Uri.parse("geo:22.638692,120.397787,3a,75y,343.8h,71.11t");	//經緯度
+//            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//            startActivity(intent);
+            Intent intent = new Intent(ActMain.this,ActMap.class);
             startActivity(intent);
+
         }
     };
 
