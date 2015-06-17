829832ae2f79f5e02678a8e6d7e4d1b16296737a
easonsu80 <easonsu80@gmail.com>
2015-06-17 14:54:33 +0800

    Myass

--- MyappTest2/app/src/main/java/com/example/danny/myapptest/ActTodo.java
+++ MyappTest2/app/src/main/java/com/example/danny/myapptest/ActTodo.java
@@ -1,17 +1,17 @@
 package com.example.danny.myapptest;
 
 import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,9 +29,7 @@ import java.util.Calendar;
 public class ActTodo extends Activity {
 
 
-    ArrayList<String> list;// = new ArrayList<String>();
-    String listSize = "0";
-            //getString(list.size());
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -116,25 +114,22 @@ public class ActTodo extends Activity {
 
 View.OnClickListener btnAdd_click =new View.OnClickListener(){
     public void onClick(View arg0) {
-        if(txtTodo.length()==0){
-                  Toast.makeText(ActTodo.this,"您尚未輸入待辨事項",Toast.LENGTH_SHORT).show();
-        }else {
-            SharedPreferences table = getSharedPreferences("T", 0);
-            int index = table.getInt("COUNT", 0);
-            index++;
-            table.edit().putInt("COUNT", index).commit();
 
-            String ketT = "T" + String.valueOf(index);
-            String ketD = "D" + String.valueOf(index);
-            table.edit().putString(ketT, txtTodo.getText().toString()).commit();
-            table.edit().putString(ketD, txtDatePicker.getText().toString()).commit();
+        SharedPreferences table = getSharedPreferences("T", 0);
+        int index = table.getInt("COUNT", 0);
+        index++;
+        table.edit().putInt("COUNT",index).commit();
 
-            Toast.makeText(ActTodo.this, "新增資料成功", Toast.LENGTH_SHORT).show();
+        String ketT="T"+String.valueOf(index);
+        String ketD="D"+String.valueOf(index);
+        table.edit().putString(ketT,txtTodo.getText().toString()).commit();
+        table.edit().putString(ketD,txtDatePicker.getText().toString()).commit();
 
-            txtDatePicker.setText("");
-            txtTodo.setText("");
-            initTextType();
-        }
+        Toast.makeText(ActTodo.this,"新增資料成功",Toast.LENGTH_SHORT).show();
+
+        txtDatePicker.setText("");
+        txtTodo.setText("");
+
     }
 };
 
@@ -142,16 +137,15 @@ View.OnClickListener btnAdd_click =new View.OnClickListener(){
 View.OnClickListener btnList_click=new View.OnClickListener(){
     public void onClick(View arg0) {
 
-        final SharedPreferences table = getSharedPreferences("T", 0);
-        final int index = table.getInt("COUNT", 0);
+        SharedPreferences table = getSharedPreferences("T", 0);
+        int index = table.getInt("COUNT", 0);
 
         if(index==0){
             Toast.makeText(ActTodo.this,"沒有工作",Toast.LENGTH_SHORT).show();
             return;
         }
 
-        ////////////////原本ArrayList位置
-        list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<String>();
         for(int i=1;i<=index;i++){
             String ketT="T"+String.valueOf(i);
             String ketD="D"+String.valueOf(i);
@@ -170,76 +164,12 @@ View.OnClickListener btnList_click=new View.OnClickListener(){
             return;
         }
 
-        final AlertDialog.Builder build=new AlertDialog.Builder(ActTodo.this);
+        AlertDialog.Builder build=new AlertDialog.Builder(ActTodo.this);
         build.setTitle("未完成工作列表");
-        build.setPositiveButton("返回",null);
-        build.setItems(list.toArray(new String[list.size()]), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                AlertDialog.Builder askDelete = new AlertDialog.Builder(ActTodo.this)
-                        .setTitle("確定要刪除？")
-                        .setMessage(list.get(which))
-                        .setPositiveButton("確定", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // list.remove(which);
-                                Toast.makeText(ActTodo.this, "你刪除了一筆備忘", Toast.LENGTH_SHORT).show();
-                                //重新排列table ListArray
-                               resetList();
+        build.setItems(list.toArray(new String[list.size()]),null).create().show();
 
-                                    //////////
-                                    // list.remove(2);
-//                                ArrayList<String> temList = new ArrayList<String>();
-//                                temList = list;
-//                                temList.remove(2);
-
-
-                                }
-                            }
-
-                            )
-
-
-                            .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            build.create().show();
-                                        }
-                                    }
-
-                            );
-                            askDelete.create().
-
-                            show();
-
-
-//                Toast.makeText(ActTodo.this,list.get(which),Toast.LENGTH_SHORT).show();
-
-//                String message="";
-//                for(int i=0;i<list.size();i++){
-//
-//
-//                }
-//                message = list.get(which);
-
-
-                        }
-            }
-
-            )
-
-
-                    .
-
-            create()
-
-            .
-
-            show();
-
-
-        }
-    };
+    }
+};
 
 DatePickerDialog.OnDateSetListener btnDateSetting_click=new DatePickerDialog.OnDateSetListener(){
 
@@ -250,10 +180,6 @@ DatePickerDialog.OnDateSetListener btnDateSetting_click=new DatePickerDialog.OnD
                 String.valueOf(year)+"-"+
                 String.valueOf(monthOfYear+1)+"-"+
                 String.valueOf(dayOfMonth) );
-                //選擇完日期
-                txtDatePicker.setTextColor(Color.BLACK);
-                txtDatePicker.setTypeface(Typeface.DEFAULT);
-                txtDatePicker.setTextSize(30);
     }
 };
 
@@ -270,58 +196,12 @@ View.OnClickListener txtDatePicker_click =new View.OnClickListener(){
                 today.get(Calendar.YEAR),
                 today.get(Calendar.MONTH),
                 today.get(Calendar.DATE)
-
-
         );
 
         dialog.show();
 
     }
 };
-    
-    private void initTextType(){
-        //預設當天
-        Calendar today=Calendar.getInstance();
-        int year = today.get(Calendar.YEAR);
-        int monthOfYear = today.get(Calendar.MONTH);
-        int dayOfMonth = today.get(Calendar.DATE);
-        txtDatePicker.setText(""+
-                String.valueOf(year)+"-"+
-                String.valueOf(monthOfYear+1)+"-"+
-                String.valueOf(dayOfMonth) );
-        //載入時字體顏色樣式
-        txtDatePicker.setTextColor(Color.GRAY);
-        txtDatePicker.setTypeface(Typeface.defaultFromStyle(3),Typeface.ITALIC);
-        txtDatePicker.setTextSize(18);
-    }
-
-
-    private void resetList(){
-
-        SharedPreferences tempTable = getSharedPreferences("T", 0);
-         int index = tempTable.getInt("COUNT", 0);
-
-        if(index==0){
-            Toast.makeText(ActTodo.this,"沒有工作",Toast.LENGTH_SHORT).show();
-            return;
-        }
-        ArrayList<String> temList = new ArrayList<String>();
-        for (int i = 1; i <= index; i++) {
-            String ketT = "T" + String.valueOf(i);
-            String ketD = "D" + String.valueOf(i);
-
-            if (tempTable.contains(ketT)) {
-
-                temList.add(
-                        tempTable.getString(ketT, "No Data") + "\r\n" +
-                                tempTable.getString(ketD, "No Data"));
-
-
-            }
-        }
-
-
-    }
 
 
     private void InitialComponent(){
@@ -335,9 +215,6 @@ View.OnClickListener txtDatePicker_click =new View.OnClickListener(){
 
 
         txtTodo = (EditText)findViewById(R.id.txtTodo);
-        initTextType();
-       
-
 
 
     }
